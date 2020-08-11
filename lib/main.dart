2897672026bd8a5c3629a3bb281faeda4f4b19a4
samuelematias <hi@samuelematias.com>
2020-08-11 11:55:58 -0300

    feat(stream): add Un/Subscribe and Emit value

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,26 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  StreamController<double> controller = StreamController<double>();
+  StreamSubscription<double> streamSubscription;
+
+  @override
+  void dispose() {
+    controller?.close();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,53 +29,38 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      home: Scaffold(
+        body: Center(
+          child: Wrap(
+            children: [
+              MaterialButton(
+                child: Text('Subscribe'),
+                color: Colors.yellow,
+                onPressed: () {
+                  // Stream the controller manages
+                  Stream stream = controller.stream;
+                  streamSubscription = stream.listen((value) {
+                    print('Value from the controller $value');
+                  });
+                },
+              ),
+              MaterialButton(
+                child: Text('Emit value'),
+                color: Colors.blue[200],
+                onPressed: () {
+                  controller.add(12);
+                },
+              ),
+              MaterialButton(
+                child: Text('Unsubscribe'),
+                color: Colors.red[200],
+                onPressed: () {
+                  streamSubscription.cancel();
+                },
+              ),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
